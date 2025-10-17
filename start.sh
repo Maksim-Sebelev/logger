f548d9f0715f5e141d198fd8556db266ea50ce1c
Maksim-Sebelev <7905111@list.ru>
2025-10-17 22:58:33 +0300

    ZoV Fixed cmake bug: no given log_dir as compile option

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf build/
 # cmake -G "Ninja" -S ./Src/ -B ./build/ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug
-cmake -G "Ninja" -S Src/ -B build/ -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug
-# cmake -G "Ninja" -S ./Src/ -B ./build/ -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release
+# cmake -G "Ninja" -S Src/ -B build/ -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug
+cmake -G "Ninja" -S ./Src/ -B ./build/ -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release
 cmake --build build/
